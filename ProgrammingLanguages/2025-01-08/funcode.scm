6dc9142a90d7af06b9e11abf1afab8f0ed36a1c1
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-01-08 14:41:32 -0600

    Scripted auto-commit on change (2025-01-08 14:41:32) by gitwatch.sh

--- ProgrammingLanguages/2025-01-08/funcode.scm
+++ ProgrammingLanguages/2025-01-08/funcode.scm
@@ -2,6 +2,16 @@
   (lambda (x y)
     (+ x y)))
 
+;;; How to write recursive functions
+;;;
+;;; Base case: easy simple approach for a small
+;;; problem
+;;;
+;;; Recursive step: how can you find a way
+;;; to get the solution using a call to
+;;; a simpler version of the problem?
+
+
 ;;; Comment
 ;;; Find the length of a list
 (define list-length

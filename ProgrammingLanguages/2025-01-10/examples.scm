ff641bd465bee0ecd0272d66a77d90750a106778
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-01-10 14:27:17 -0600

    Scripted auto-commit on change (2025-01-10 14:27:17) by gitwatch.sh

---
+++ ProgrammingLanguages/2025-01-10/examples.scm
@@ -0,0 +1,9 @@
+;; Get the nth item of a list
+(define nth
+  (lambda (lst n)
+    (if (null? lst)
+	#f  ; empty list: no item, so just false
+	(if (equal? n 0)
+	    (car lst)
+	    (nth lst (- n 1))))))
+	

5dd8ea7f2b4480cf4edefd3be0fa494cb06ee282
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-01-10 14:49:57 -0600

    Scripted auto-commit on change (2025-01-10 14:49:57) by gitwatch.sh

--- ProgrammingLanguages/2025-01-10/examples.scm
+++ ProgrammingLanguages/2025-01-10/examples.scm
@@ -27,6 +27,13 @@
 	  (else (cons (car lst)
 		      (remove-first item (cdr lst)))))))
 
+(define remove
+  (lambda (item lst)
+    (cond ((null? lst) '())
+	  ((equal? item (car lst))
+	   (remove item (cdr lst)))
+	  (else (cons (car lst)
+		      (remove item (cdr lst)))))))
 
 
 

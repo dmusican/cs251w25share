de2e762b3d49984b60b427a2002313e4bf9fe38c
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-01-13 14:25:47 -0600

    Scripted auto-commit on change (2025-01-13 14:25:47) by gitwatch.sh

--- ProgrammingLanguages/2025-01-13/examples.scm
+++ ProgrammingLanguages/2025-01-13/examples.scm
@@ -2,3 +2,11 @@
   (lambda (a)
     (lambda (b)
       (* a b))))
+
+;; map
+;; "distributing" (except that Scheme on our laptops doesn't do that)
+(define addOne
+  (lambda (x)
+    (+ x 1)))
+
+(map addOne '(9 1 2 3 7))

68feee6c464ba14c6cd72bab2f5281f35b94185d
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-01-15 14:26:39 -0600

    Scripted auto-commit on change (2025-01-15 14:26:39) by gitwatch.sh

--- ProgrammingLanguages/2025-01-15/Thing.java
+++ ProgrammingLanguages/2025-01-15/Thing.java
@@ -6,5 +6,16 @@ public class Thing {
         System.out.println(Arrays.toString(words));
         Arrays.sort(words);
         System.out.println(Arrays.toString(words));
+        // Sort by length
+        // Second parameter is a function of
+        // TWO parameters, which are two
+        // different words in list, and we should
+        // return a postive value if the first
+        // word is greater, 0 if same, negative
+        // if second is greater
+        Arrays.sort(words,
+                    (a, b) -> a.length() - b.length());
+
+        System.out.println(Arrays.toString(words));
     }
 }

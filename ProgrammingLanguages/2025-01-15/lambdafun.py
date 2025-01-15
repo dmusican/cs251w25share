4d154a029bbcc256b312b384cb4d93effef6f4f0
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-01-15 14:15:51 -0600

    Scripted auto-commit on change (2025-01-15 14:15:51) by gitwatch.sh

--- ProgrammingLanguages/2025-01-15/lambdafun.py
+++ ProgrammingLanguages/2025-01-15/lambdafun.py
@@ -13,3 +13,8 @@ items = [('horse', 9), ('cow', 3),
          ('zebra', 0)]
 
 print(sorted(items))
+# key is a function on each item you're
+# sorting that indicates what you are
+# sorting it by
+print(sorted(items,
+             key=lambda animal: animal[1]))

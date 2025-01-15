e2ef7c37c57a57292a8408012538f319c3d5c4a2
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-01-15 14:13:07 -0600

    Scripted auto-commit on change (2025-01-15 14:13:07) by gitwatch.sh

--- ProgrammingLanguages/2025-01-15/lambdafun.py
+++ ProgrammingLanguages/2025-01-15/lambdafun.py
@@ -6,3 +6,8 @@ print( (lambda x: x+1)(5) )
 
 adder = lambda x: x+1
 print(adder(5))
+
+# Tons of applications, the obvious one
+# is sorting
+items = [('horse', 9), ('cow', 3),
+         ('zebra', 0)]

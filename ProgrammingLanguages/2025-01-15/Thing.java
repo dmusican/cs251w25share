c36765836d50e391b33ec8614387e9a6244d0028
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-01-15 14:22:06 -0600

    Scripted auto-commit on change (2025-01-15 14:22:06) by gitwatch.sh

---
+++ ProgrammingLanguages/2025-01-15/Thing.java
@@ -0,0 +1,7 @@
+public class Thing {
+    public static void main(String[] args) {
+        String[] words = {"a", "horse", "is",
+                          "happiest", "now"};
+        System.out.println(Arrays.toString(words));
+    }
+}

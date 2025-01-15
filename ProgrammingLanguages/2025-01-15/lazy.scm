da62ab3bd1325b76f720359916fcea70f91986f1
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-01-15 13:55:08 -0600

    Scripted auto-commit on change (2025-01-15 13:55:08) by gitwatch.sh

--- ProgrammingLanguages/2025-01-15/lazy.scm
+++ ProgrammingLanguages/2025-01-15/lazy.scm
@@ -1,1 +1,7 @@
-
+(define gen-lazy-list
+  (lambda (start stop)
+    (if (> start stop)
+        '()
+        (cons start
+            (lambda ()
+              (gen-lazy-list (+ start 1) stop))))))

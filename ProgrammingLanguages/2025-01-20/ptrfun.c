3fdae39cea080a1ffe95959604bd94fe01c25589
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-01-20 14:26:49 -0600

    Scripted auto-commit on change (2025-01-20 14:26:49) by gitwatch.sh

--- ProgrammingLanguages/2025-01-20/ptrfun.c
+++ ProgrammingLanguages/2025-01-20/ptrfun.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+  // 3 is in the stack
   int x = 3;
   printf("%i\n", x);
+
+  // allocate memory in the heap big enough
+  // to hold an int
+  //intptr y = malloc(sizeof(int));
+  int *y = malloc(sizeof(int));
+
+  // Put a 7 in the heap location we point to
+  *y = 7;
+  // ^^^ Follow the pointer in y to the memory
+  // location, and put a 7 in it
 }

38ffaf29b35c0f2b165f486a03f5f86237dd90f8
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-01-27 14:06:42 -0600

    Scripted auto-commit on change (2025-01-27 14:06:42) by gitwatch.sh

--- ProgrammingLanguages/2025-01-27/stringyfun.c
+++ ProgrammingLanguages/2025-01-27/stringyfun.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
     // String containing hello
@@ -8,6 +10,14 @@ int main() {
     word[2] = 'l';
     word[3] = 'l';
     word[4] = 'o';
-    // word[5] = '\0';
+    word[5] = '\0';
     printf("%s\n", word);
+
+    // Longer than I need, but I can
+    char *word2 = malloc(sizeof(char) * 10);
+    // Faster way to initialize
+    strcpy(word2, "hello");
+
+
+
 }

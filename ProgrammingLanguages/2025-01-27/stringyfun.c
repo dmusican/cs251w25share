f35a4f8423b6628e4b8df65aa9bde61209eb7490
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-01-27 13:56:57 -0600

    Scripted auto-commit on change (2025-01-27 13:56:57) by gitwatch.sh

---
+++ ProgrammingLanguages/2025-01-27/stringyfun.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // String containing hello
+    char word[6];
+    word[0] = 'h';
+    word[1] = 'e';
+    word[2] = 'l';
+    word[3] = 'l';
+    word[4] = 'o';
+    word[5] = '\0';
+    printf("%s\n", word);
+}

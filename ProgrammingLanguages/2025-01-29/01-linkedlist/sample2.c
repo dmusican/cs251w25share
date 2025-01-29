57cfbde930498911b232725fe4c94e391a2af640
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-01-29 14:04:43 -0600

    Scripted auto-commit on change (2025-01-29 14:04:43) by gitwatch.sh

--- ProgrammingLanguages/2025-01-29/01-linkedlist/sample2.c
+++ ProgrammingLanguages/2025-01-29/01-linkedlist/sample2.c
@@ -1,8 +1,11 @@
 #include "schemeval.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 int main() {
 
     SchemeVal *value = malloc(sizeof(SchemeVal));
+    value->i = 3;
+    printf("value is %i\n", value->i);
 
 }

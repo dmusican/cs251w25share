ad9daa03750a390b1e53034d69c9558b1f6b00b4
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-01-29 13:53:58 -0600

    Scripted auto-commit on change (2025-01-29 13:53:58) by gitwatch.sh

--- ProgrammingLanguages/2025-01-29/sample.c
+++ ProgrammingLanguages/2025-01-29/sample.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+// questionable strategy (but the right one)
+// for handling multiple types with the same variable
+
+union Thingydoo {
+    int x;
+    double x;
+};
+
 int main() {
-    int x = 3;
-    double x = 9.7;
+    // int x = 3;
+    // double x = 9.7;
 }

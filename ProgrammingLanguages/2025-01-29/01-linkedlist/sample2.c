19b3464686c4a2eaa001907f9f9b1c733a0d611d
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-01-29 14:08:05 -0600

    Scripted auto-commit on change (2025-01-29 14:08:05) by gitwatch.sh

--- ProgrammingLanguages/2025-01-29/01-linkedlist/sample2.c
+++ ProgrammingLanguages/2025-01-29/01-linkedlist/sample2.c
@@ -5,8 +5,13 @@
 int main() {
 
     SchemeVal *value = malloc(sizeof(SchemeVal));
+    value->type = INT_TYPE;
     value->i = 3;
-    printf("value is %i\n", value->i);
+    if (value->type == INT_TYPE) {
+        printf("value is %i\n", value->i);
+    } else if (value->type == DOUBLE_TYPE) {
+        printf("value is %g\n", value->d);
+    }
 
     value->car = value;
     value->cdr = value;

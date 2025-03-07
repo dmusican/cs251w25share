80b4dc3803b1bb39fb6c90fd8f0f93fed75228ec
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-03-07 14:24:11 -0600

    Scripted auto-commit on change (2025-03-07 14:24:11) by gitwatch.sh

--- ProgrammingLanguages/2025-03-07/let-games.scm
+++ ProgrammingLanguages/2025-03-07/let-games.scm
@@ -1,2 +1,10 @@
-(let ((a 3) (b (+ a 1)))
-  b)
+;; (let ((a 3) (b (+ a 1)))
+;;   b)
+;; Bad because a not bound yet when try to add 1
+
+;; new thing
+;; temporary helper function
+(define add-one
+  (lambda (x)
+    (+ x 1)))
+(add-one 5)
